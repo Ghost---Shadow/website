93b383ae5509958f4732b6ec57f4cb659556cc33
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2022-09-10 18:47:42 +0200

    Add eslint and mock markdown

--- craco.config.js
+++ craco.config.js
@@ -1,13 +1,14 @@
-const { addAfterLoader, loaderByName } = require('@craco/craco')
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { addAfterLoader, loaderByName } = require('@craco/craco');
 
 module.exports = {
   webpack: {
     configure(webpackConfig) {
       addAfterLoader(webpackConfig, loaderByName('babel-loader'), {
         test: /\.mdx?$/,
-        loader: require.resolve('@mdx-js/loader')
-      })
-      return webpackConfig
-    }
-  }
-}
+        loader: require.resolve('@mdx-js/loader'),
+      });
+      return webpackConfig;
+    },
+  },
+};
