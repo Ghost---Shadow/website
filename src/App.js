663a86d1247c6c34626c82022cfbd79240ddb910
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2022-09-10 18:22:57 +0200

    Basic MDX setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Content from './content.mdx'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Content />
     </div>
   );
 }
