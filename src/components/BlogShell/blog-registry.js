c778c081aafc64d63c0369f2f906a3ae63a81cd8
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2022-09-18 19:06:16 +0200

    Ported UTR

--- src/components/BlogShell/blog-registry.js
+++ src/components/BlogShell/blog-registry.js
@@ -7,10 +7,10 @@ export const blogRegistry = [{
   date: '20 Feb 2022',
 },
 {
-  slug: 'page1',
-  title: 'Lorem Ipsum 1',
-  component: React.lazy(() => import('../../blog/page1.mdx')),
-  date: '19 Feb 2022',
+  slug: 'unit-test-recorder',
+  title: 'Unit Test Recorder - Automatically generate unit tests as you use your application',
+  component: React.lazy(() => import('../../blog/unit-test-recorder.mdx')),
+  date: '25 Jun 2020',
 }];
 
 export const slugToId = blogRegistry.reduce((acc, next, idx) => ({
