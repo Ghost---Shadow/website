fbfe9d24d5888bed8c6de253aec95dcdb4b46dda
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2022-09-24 17:23:48 +0200

    Global styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { MantineProvider } from '@mantine/core';
 import BlogPage from './pages/BlogPage';
 import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/blog" element={<BlogPage />} />
-        <Route path="/blog/:slug" element={<BlogPage />} />
-        <Route path="*" element={<LandingPage />} />
-      </Routes>
-    </BrowserRouter>
+    <MantineProvider withGlobalStyles>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/blog/:slug" element={<BlogPage />} />
+          <Route path="*" element={<LandingPage />} />
+        </Routes>
+      </BrowserRouter>
+    </MantineProvider>
   );
 }
 
