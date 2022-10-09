19a660cf7d227eb3a3670bc3214a6e7f59e5827b
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2022-10-09 18:34:14 +0200

    Fix highlightingn

--- src/highlight-worker.js
+++ src/highlight-worker.js
@@ -1,17 +1,23 @@
 import Prism from 'prismjs';
 
-export const highlight = (foo) => {
+export const highlight = () => {
+  document.querySelectorAll('pre').forEach((match) => {
+    // This line SOMETIMES does not execute on rerender
+    /* eslint-disable no-param-reassign */
+    match.className = 'language-markup';
+  });
+
   const matches = document.querySelectorAll('code');
   matches.forEach((match) => {
     const className = match.className || 'language-markup';
     const language = className.split('-')[1];
     /* eslint-disable no-param-reassign */
-    // This code does not execute on rerender
+
+    // This line SOMETIMES does not execute on rerender
     match.parentElement.className = match.className;
 
     match.innerHTML = Prism.highlight(match.innerText, Prism.languages[language], language);
     /* eslint-enable no-param-reassign */
   });
-  return foo;
 };
 export const foo = () => 42;
