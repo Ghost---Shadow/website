59bb6d128fe88bfffd2662120a07429af74ab83f
Souradeep Nanda <souradeep.nanda123@gmail.com>
2025-10-19 17:03:36 +0530

    UAT can solve XOR problem

--- src/components/BlogShell/blog-registry.js
+++ src/components/BlogShell/blog-registry.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 export const blogRegistry = [
   {
+    slug: 'universal-approximation-theorem-is-right',
+    title: 'The Universal Approximation Theorem Is Right. You\'re Using It Wrong.',
+    component: React.lazy(() => import('../../blog/universal-approximation-theorem-is-right/index.mdx')),
+    date: '19 Oct 2025',
+  },
+  {
     slug: 'the-power-law-illusion',
     title: 'The Power Law Illusion: A Measurement Artifact Hypothesis',
     component: React.lazy(() => import('../../blog/the-power-law-illusion/index.mdx')),
