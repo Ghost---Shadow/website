0821ed98a37de918311a052886817bd562531ceb
Souradeep Nanda <souradeep.nanda123@gmail.com>
2025-11-19 12:28:48 +0530

    Remove update version

--- scripts/updateVersion.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-const packageJson = JSON.parse(fs.readFileSync('./package.json').toString());
-const { version } = packageJson;
-
-fs.writeFileSync('./build/version.html', version);
